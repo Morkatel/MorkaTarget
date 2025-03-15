8970589e3904b6ee4e60587c7e51e37ded93517c
Morkatel <morkatel@proton.me>
2025-03-15 16:33:23 +0100

    Allow acchievement selection.

--- MorkaTarget/Frame.lua
+++ MorkaTarget/Frame.lua
@@ -3,8 +3,16 @@
 
 local GLOB_MapId;
 local GLOB_POS;
-local GLOB_Ticker;
 local GLOB_LastDist;
+
+local achievementIdList = {
+    --fistful of love:
+    1699,
+    --Turkey Lurkey:
+    3559
+}
+
+local GLOB_AchievementId;
 
 -- Function to get the distance between two map positions
 local function GetDistanceBetweenPositions(x1, y1, x2, y2)
@@ -16,14 +24,14 @@ local function GetDistanceBetweenPositions(x1, y1, x2, y2)
 end
 
 function UpdateDistance()
-    playerPos = C_Map.GetPlayerMapPosition(GLOB_MapId, "player")
+    local playerPos = C_Map.GetPlayerMapPosition(GLOB_MapId, "player")
     local distance = GetDistanceBetweenPositions(
-		playerPos.x * 100, playerPos.y * 100, GLOB_POS.x * 100, GLOB_POS.y * 100)
+        playerPos.x * 100, playerPos.y * 100, GLOB_POS.x * 100, GLOB_POS.y * 100)
     if GLOB_LastDist and distance > GLOB_LastDist then
-	    print("Further")
-	else
-	    print("Closer")
-	end
+        print("Further")
+    else
+        print("Closer")
+    end
     GLOB_LastDist = distance
     MO_Text_Distance:SetText(string.format("%.2f", distance))
 end
@@ -39,21 +47,17 @@ function MorkaTargetMain_OnLoad()
             local targetRace = UnitRace(unit)
             local targetClass = UnitClass(unit)
             local targetRaceClass = targetRace .. " " .. targetClass
-
-            --fistful of love: 1699
-            --Turkey Lurkey: 3559
-            local achievementId = 1699
-
-            local totalCriterias = GetAchievementNumCriteria(achievementId)
+            local totalCriterias = GetAchievementNumCriteria(GLOB_AchievementId)
+            Text_MO_Race:SetText(targetRace)
+            Text_MO_Class:SetText(targetClass)
             for critIdx = 1, totalCriterias, 1 do
                 local criteriaString, criteriaType, completed, quantity, reqQuantity,
-                charName, flags, assetID, quantityString, criteriaID = GetAchievementCriteriaInfo(achievementId, critIdx)
+                charName, flags, assetID, quantityString, criteriaID = GetAchievementCriteriaInfo(GLOB_AchievementId,
+                    critIdx)
 
-                Text_MO_Race:SetText(targetRace)
-                Text_MO_Class:SetText(targetClass)
-                if targetRaceClass == criteriaString then
-                    if completed == false then
-                        --SOUNDKIT:ALARM_CLOCK_WARNING_2 
+                if completed == false then
+                    if targetRaceClass == criteriaString then
+                        --SOUNDKIT:ALARM_CLOCK_WARNING_2
                         --12867
                         PlaySound(12867)
                         Text_Found_Name:SetText(UnitNameUnmodified(unit))
@@ -66,5 +70,25 @@ function MorkaTargetMain_OnLoad()
 
     EventFrame:SetScript("OnEvent", OnTargetChanged)
 
-	print("MorkaTarget loaded")
+    PopulateItemList()
+    print("MorkaTarget loaded")
+end
+
+-- Function to populate the item list
+function PopulateItemList(filterMe)
+    -- Clear existing items in a UIDropDownMenuTemplate
+    UIDropDownMenu_Initialize(DropDownFrame, function(self, level, menuList)
+        for i, item in ipairs(achievementIdList) do
+            local info = UIDropDownMenu_CreateInfo()
+            local achiName = select(2, GetAchievementInfo(item))
+            info.text = achiName
+            info.value = item
+            info.func = function(self)
+                local achiName = select(2, GetAchievementInfo(self.value))
+                UIDropDownMenu_SetText(DropDownFrame, achiName)
+                GLOB_AchievementId = self.value
+            end
+            UIDropDownMenu_AddButton(info)
+        end
+    end)
 end
